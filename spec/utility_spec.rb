eb8c671e96a736948f0818bc80fe7c70acf54d82
Calum Gilchrist <calum@homelinen.org>
2014-04-07 23:35:55 +0100

    Refactored Utility

--- spec/utility_spec.rb
+++ spec/utility_spec.rb
@@ -4,32 +4,31 @@ require 'utility'
 describe Utility, "provides utilities" do
 
     before(:each) do
-        @arry = [
-            Webpage.new('/'),
-            Webpage.new('/about'),
-            Webpage.new('/contact'),
-            Webpage.new('/about/contract'),
-        ]
+        @arry = []
+        @links = ['/', '/about', '/contact', '/about/contract']
+        @false_links = ['/friends', '/about/false']
+
+        @arry = @links.map { |l| Webpage.new(l) }
     end
 
     it "can find links as strings" do
-        Utility.has_link?(@arry, '/').should be_true
-        Utility.has_link?(@arry, '/about').should be_true
-        Utility.has_link?(@arry, '/contact').should be_true
-        Utility.has_link?(@arry, '/about/contract').should be_true
+        @links.each do |l|
+            Utility.has_link?(@arry, l).should be_true
+        end
         
-        Utility.has_link?(@arry, '/friends').should be_false
-        Utility.has_link?(@arry, '/about/friends').should be_false
+        @false_links.each do |l|
+            Utility.has_link?(@arry, l).should be_false
+        end
     end
 
     it "can find links as pages" do
 
-        Utility.has_link?(@arry, Webpage.new('/')).should be_true
-        Utility.has_link?(@arry, Webpage.new('/about')).should be_true
-        Utility.has_link?(@arry, Webpage.new('/contact')).should be_true
-        Utility.has_link?(@arry, Webpage.new('/about/contract')).should be_true
-
-        Utility.has_link?(@arry, Webpage.new('/friends')).should be_false
-        Utility.has_link?(@arry, Webpage.new('/about/friends')).should be_false
+        @links.each do |l|
+            Utility.has_link?(@arry, Webpage.new(l)).should be_true
+        end
+        
+        @false_links.each do |l|
+            Utility.has_link?(@arry, Webpage.new(l)).should be_false
+        end
     end
 end
