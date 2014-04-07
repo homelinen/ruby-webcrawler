477b11172597de0eaed2e05d07dc3397dc6369ec
Calum Gilchrist <calum@homelinen.org>
2014-04-08 00:09:36 +0100

    Removed debugging from Utility
    Nasty puts left around.

--- lib/utility.rb
+++ lib/utility.rb
@@ -2,8 +2,6 @@
 # Shared utility code
 module Utility
     def self.has_link?(ary, link)
-        puts ary
-        puts link
         if link.class == String
             ary.any? do |webpage|
                 webpage.node_name == link
