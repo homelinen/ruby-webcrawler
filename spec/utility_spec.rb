b7521c298ac138b36b5f5432adf2f4a2aa5c0390
Calum Gilchrist <calum@homelinen.org>
2014-04-08 11:22:31 +0100

    Added a Test to Identify Array of String issue in has_links

--- spec/utility_spec.rb
+++ spec/utility_spec.rb
@@ -32,3 +32,33 @@ describe Utility, "provides utilities" do
         end
     end
 end
+
+describe Utility, "string arrays" do
+    before(:each) do
+        @links = ['/', '/about', '/contact', '/about/contract']
+        @false_links = ['/friends', '/about/false']
+
+        @arry = @links
+    end
+
+    it "can find links as strings" do
+        @links.each do |l|
+            Utility.has_link?(@arry, l).should be_true
+        end
+        
+        @false_links.each do |l|
+            Utility.has_link?(@arry, l).should be_false
+        end
+    end
+
+    it "can find links as pages" do
+
+        @links.each do |l|
+            Utility.has_link?(@arry, Webpage.new(l)).should be_true
+        end
+        
+        @false_links.each do |l|
+            Utility.has_link?(@arry, Webpage.new(l)).should be_false
+        end
+    end
+end
